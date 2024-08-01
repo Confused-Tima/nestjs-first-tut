c077d109369a70cedc53cf01c7222db1c89116d4
Confused <thsibtima1999@gmail.com>
2024-08-01 23:50:57 +0530

    Controller services and dtos added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInData } from './auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authSerivce: AuthService) {}
 
-  @Post('signin')
-  signup() {
+  @Post('signup')
+  signup(@Body() data: SignInData) {
+    console.log(data.email, data.password)
     return this.authSerivce.signup();
   }
 
-  @Post('signup')
+  @Post('signin')
   signin() {
     return this.authSerivce.signin();
   }
